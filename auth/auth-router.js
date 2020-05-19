65b101dff31d4943c42283650a4ea8fe4d454dea
evan <aspaasevan@gmail.com>
2020-05-19 13:29:37 -0700

    register and login successful

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,5 @@
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = require("express").Router();
 
@@ -15,8 +16,6 @@ router.post("/register", (req, res) => {
     const hash = bcryptjs.hashSync(credentials.password, rounds);
 
     credentials.password = hash;
-
-    // TODO token
 
     // save the user to the database
     Users.add(credentials)
@@ -37,11 +36,14 @@ router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   if (isValid(req.body)) {
-    Users.findBy({ username: username })
+    Users.findBy({ "username": username })
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          // produce and send a token that includes the username and the role of the user
+          const token = createToken(user);
+
+          res.status(200).json({ message: "Welcome to our API", token });
         } else {
           res.status(401).json({ message: "Invalid credentials" });
         }
@@ -55,5 +57,21 @@ router.post("/login", (req, res) => {
     });
   }
 });
+
+function createToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const secret = process.env.JWT_SECRET || "keepitsecret,keepitsafe!";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
